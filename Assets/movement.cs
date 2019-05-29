0cd7ac9a32414da78704df9b7ed4cbc559e0bf32
Freeman12321 <freeman.abrahams@s.elim.school.nz>
2019-05-29 21:38:27 +1200

    movement of player

---
+++ Assets/movement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour {
+    public Rigidbody rb;
+    public float forwardForce = 50f;
+    public float sidewaysForce = 50f;
+  
+	// Update is called once per frame
+	void Update () {
+        rb.AddForce(forwardForce, 0, 0);
+        if (Input.GetKey("d"))
+        {
+            rb.AddForce(0, 0, -sidewaysForce);
+        }
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(0, 0, sidewaysForce);
+        }
+    }
+}
