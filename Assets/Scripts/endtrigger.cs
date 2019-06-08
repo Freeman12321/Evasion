6956f3e4b7cefdf1f4c22b4dc8c4b4b4d6b05ea4
Freeman12321 <freeman.abrahams@s.elim.school.nz>
2019-06-08 14:18:46 +1200

    winning levels

---
+++ Assets/Scripts/endtrigger.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class endtrigger : MonoBehaviour {
+
+    public gamemanagement gameManager;
+    public void OnTriggerEnter()
+    {
+        gameManager.CompleteLevel();
+    }
+}
---
+++ Assets/Scripts/gamemanagement.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+public class gamemanagement : MonoBehaviour {
+
+    bool gameHasEnded = false;
+
+    public float restartDelay = 1f;
+
+    public GameObject completeLevelUI;
+
+    public void CompleteLevel() {
+        completeLevelUI.SetActive(true);
+    }
+
+    public void endGame() {
+
+        //lose a life and only end the game once
+        if (gameHasEnded == false) {
+            gameHasEnded = true;
+            Invoke("Restart", restartDelay);
+        }
+    }
+    public void Restart() {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
+
